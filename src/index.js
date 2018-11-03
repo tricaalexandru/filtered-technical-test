c8a2b6fd7fa736ecee46cb474bbac75dffcd789c
mikehurl <mikehurl@gmail.com>
2018-11-03 13:34:44 +0000

    intial redux boilerplate created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/globalCSS';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
