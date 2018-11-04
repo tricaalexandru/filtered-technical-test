eb988b6c59e4379a21290032ff5d5cd6593c1e06
mikehurl <mikehurl@gmail.com>
2018-11-04 20:19:55 +0000

    constructed App to render created containers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './reducers';
+import reducers from './reducers';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/globalCSS';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
