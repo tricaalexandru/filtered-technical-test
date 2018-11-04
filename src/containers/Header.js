25be0f104b0d82a5013633abcc60930f2c06a797
mikehurl <mikehurl@gmail.com>
2018-11-04 20:41:58 +0000

    ensured Header props are either a number or null instead of evaluating to a number or boolean

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,20 +5,31 @@ const mapStateToProps = ({ previewReducer }) => ({
   numberOfArticles:
     previewReducer.assetList &&
     previewReducer.assetList.length > 0 &&
-    previewReducer.assetList.filter(asset => asset.type === 'Article').length,
+    previewReducer.assetList.filter(asset => asset.type === 'Article').length
+      ? previewReducer.assetList.filter(asset => asset.type === 'Article')
+          .length
+      : null,
   numberOfDocuments:
     previewReducer.assetList &&
     previewReducer.assetList.length > 0 &&
-    previewReducer.assetList.filter(asset => asset.type === 'Document').length,
+    previewReducer.assetList.filter(asset => asset.type === 'Document').length
+      ? previewReducer.assetList.filter(asset => asset.type === 'Document')
+          .length
+      : null,
   numberOfOnlineCourses:
     previewReducer.assetList &&
     previewReducer.assetList.length > 0 &&
     previewReducer.assetList.filter(asset => asset.type === 'Online Course')
-      .length,
+      .length
+      ? previewReducer.assetList.filter(asset => asset.type === 'Online Course')
+          .length
+      : null,
   numberOfVideos:
     previewReducer.assetList &&
     previewReducer.assetList.length > 0 &&
-    previewReducer.assetList.filter(asset => asset.type === 'Video').length,
+    previewReducer.assetList.filter(asset => asset.type === 'Video').length
+      ? previewReducer.assetList.filter(asset => asset.type === 'Video').length
+      : null,
   totalNumberOfAssets: previewReducer.assetList
     ? previewReducer.assetList.length
     : 0
